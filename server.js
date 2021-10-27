eba64efc860be99e45e31c42d161a98e95843f78
Sean Craig <seancraig21@gmail.com>
2021-10-26 19:13:11 -0500

    get request for api/notes working

--- server.js
+++ server.js
@@ -11,19 +11,20 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.get('/api/notes', (req, res) => {
-  console.info(`GET api/notes`);
-  res.status(200).json(db);
+  res.json(db);
+  
+  console.info(`GET request for notes received`);
 });
 
 app.listen(PORT, () =>
